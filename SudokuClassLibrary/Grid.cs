8a9e49748ce8cd972a2310687797ae939a4f045a
SimonE <SimonE@datacom.co.nz>
2022-12-12 23:57:46 +1300

    Turn off IsInitialValue before resetting Cell Value
    Otherwise, if IsInitialValue set, the cell will prevent the value from
    being changed.

--- SudokuClassLibrary/Grid.cs
+++ SudokuClassLibrary/Grid.cs
@@ -54,6 +54,9 @@ namespace SudokuClassLibrary
             { 
                 foreach (var cell in this.Cells)
                 {
+                    // Can't set or clear a cell set to an initial value.
+                    // So ensure IsInitialValue is turned off before clearing the value.
+                    cell.IsInitialValue = false;
                     cell.Value = null;
                 }
 
